07203be8b2d21f0646c0504ae62d915984433414
Jim Hopp <jimhopp@gmail.com>
2017-08-18 15:26:57 -0700

    Initial Commit

---
+++ Flight Risk Assessment ToolTests/Flight_Risk_Assessment_ToolTests.swift
@@ -0,0 +1,36 @@
+//
+//  Flight_Risk_Assessment_ToolTests.swift
+//  Flight Risk Assessment ToolTests
+//
+//  Created by Jim Hopp on 8/18/17.
+//  Copyright © 2017 Jim Hopp. All rights reserved.
+//
+
+import XCTest
+@testable import Flight_Risk_Assessment_Tool
+
+class Flight_Risk_Assessment_ToolTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
